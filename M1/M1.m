2546e81b7c1e57ab8de774cae8d10210042b3476
Ádám Domaföldi <adam@domafoldi.hu>
2018-04-12 23:07:03 +0200

    Add more plots

--- M1/M1.m
+++ M1/M1.m
@@ -7,40 +7,25 @@ adamRest=hhmbinread('rest/adam_relaxed.hhm');
 zsofiaRest=hhmbinread('rest/zsofia_relaxed.hhm');
 brigittaRest=hhmbinread('rest/brigitta_relaxed.hhm');
 
-% % this transformation applies to the „2” signed HHMD 
-% % .ecg1 is a property of the object (for instance: adamRest) and holds the
-% % value of Einthoven I EKG signals
-% adamRestMv = 3.3 / 4096 *(adamRest.ecg1 - 2048);
-% zsofiaRestMv = 3.3 / 4096 *(zsofiaRest.ecg1 - 2048);
-% brigittaRestMv = 3.3 / 4096 *(brigittaRest.ecg1 - 2048);
-% 
-% % this transformation applies to the „1” signed HHMD 
-adamRestMv = 3.3 / 8192 * (adamRest.ecg1 - 2048);
-zsofiaRestMv = 3.3 / 8192 *(zsofiaRest.ecg1 - 2048);
-brigittaRestMv = 3.3 / 8192 *(brigittaRest.ecg1 - 2048); 
-% 
+% this transformation applies to the „1” signed HHMD 
+% .ecg1 is a property of the object (for instance: adamRest) and holds the
+% value of Einthoven I EKG signals
+adamRestMv = toMillivolt(adamRest.ecg1);
+zsofiaRestMv = toMillivolt(zsofiaRest.ecg1);
+brigittaRestMv = toMillivolt(brigittaRest.ecg1); 
+
 fs=1000; % sampled at this frequency [Hz]
  
 % % visualize data
+% % this plots the raw signal
 % figure()
 % t=(1:length(brigittaRestMv))/fs; % time axis [sec]
 % plot(t,brigittaRestMv)
 
 %% 1/a Filter and visualize signals
 
-% filter signals using 5th-order Butterworth filter, 0.5 Hz lower and 40Hz
-% upper cutoff frequencies
-%adamRestMvFiltered = butterworthFilter(adamRestMv,5,40,0.5);
-%zsofiaRestMvFiltered = butterworthFilter(zsofiaRestMv,5,40,0.5);
-%brigittaRestMvFiltered = butterworthFilter(brigittaRestMv,5,40,0.5);
-
-% visualize filtered signal
-% figure()
-% t=(1:length(brigittaRestMvFiltered))/fs; % time axis [sec]
-% plot(t,brigittaRestMvFiltered)
-
-% filter signals using 5th-order Butterworth filter, 0.5 Hz upper and 20Hz
-% lower cutoff frequencies
+% filter signals using 5th-order Butterworth filter, 0.5 Hz upper and
+% 20Hz-30Hz lower cutoff frequencies
 adamRestMvFiltered = butterworthFilter(adamRestMv,5,20,0.5);
 zsofiaRestMvFiltered = butterworthFilter(zsofiaRestMv,5,30,0.5);
 brigittaRestMvFiltered = butterworthFilter(brigittaRestMv,5,20,0.5);
@@ -48,26 +33,26 @@ brigittaRestMvFiltered = butterworthFilter(brigittaRestMv,5,20,0.5);
 % % visualize filtered signal
 % figure()
 % t=(1:length(adamRestMvFiltered))/fs; % time axis [sec]
-% subplot(3,1,1)
+% subplot(2,1,1)
 % plot(t,adamRestMv)
-% title('Adam rest')
-% subplot(3,1,2)
+% title('Adam rest - filtered EKG signal')
+% subplot(2,1,2)
 % plot(t,adamRestMvFiltered)
 % 
 % figure()
 % t=(1:length(zsofiaRestMvFiltered))/fs; % time axis [sec]
-% subplot(4,1,1)
+% subplot(3,1,1)
 % plot(t,zsofiaRestMv)
-% title('Zsofia rest')
-% subplot(4,1,2)
+% title('Zsofia rest - filtered EKG signal')
+% subplot(3,1,2)
 % plot(t,zsofiaRestMvFiltered)
 % 
 % figure()
 % t=(1:length(brigittaRestMvFiltered))/fs; % time axis [sec]
-% subplot(7,1,1)
+% subplot(4,1,1)
 % plot(t,brigittaRestMv)
-% title('Brigitta rest')
-% subplot(3,1,2)
+% title('Brigitta rest - filtered EKG signal')
+% subplot(4,1,2)
 % plot(t,brigittaRestMvFiltered)
 
 % split signals
@@ -76,135 +61,141 @@ zsofiaRestMvFilteredSplit = zsofiaRestMvFiltered(40000:80000);
 brigittaRestMvFilteredSplit = brigittaRestMvFiltered(10000:50000);
 
 % visualize split signals
-% t=(1:length(adamRestMvFilteredSplit))/fs; % all three signals have the same length by now
-% figure()
-% subplot(3,1,1) %(3 rows, 1th element)
-% plot(t,adamRestMvFilteredSplit); title('Adam rest');xlabel('t [sec]');ylabel('U [mV]');
-% subplot(3,1,2)
-% plot(t,zsofiaRestMvFilteredSplit); title('Zsofia rest');xlabel('t [sec]');ylabel('U [mV]');
-% subplot(3,1,3)
-% plot(t,brigittaRestMvFilteredSplit); title('Brigitta rest');xlabel('t [sec]');ylabel('U [mV]');
+t=(1:length(adamRestMvFilteredSplit))/fs; % all three signals have the same length (40 sec) by now
+
+figure()
+subplot(5,1,1) %(3 rows, 1th element)
+plot(t,adamRestMvFilteredSplit);
+title('Adam rest - filtered and split EKG signal');xlabel('t [sec]');ylabel('U [mV]');
+
+subplot(5,1,2)
+plot(t,zsofiaRestMvFilteredSplit);
+title('Zsofia rest - filtered and split EKG signal');xlabel('t [sec]');ylabel('U [mV]');
+
+subplot(5,1,3)
+plot(t,brigittaRestMvFilteredSplit);
+title('Brigitta rest - filtered and split EKG signal');xlabel('t [sec]');ylabel('U [mV]');
 
 %% 1/b Visualize in frequency domain
+% fast fourier transform ekg signals and visualize them, original filtered
+% signal is used, no split
+frequencySpectrum(adamRestMvFiltered,'Adam rest - filtered EKG frequency spectrum (original length)');
+frequencySpectrum(zsofiaRestMvFiltered,'Zsofia rest - filtered EKG frequency spectrum (original length)');
+frequencySpectrum(brigittaRestMvFiltered,'Brigitta rest - filtered  EKG frequency spectrum (original length)'); 
 
-%frequencySpectrum(adamRestMvFilteredSplit,'Adam rest fft');
-%frequencySpectrum(zsofiaRestMvFilteredSplit,'Zsofia rest fft');
-%frequencySpectrum(brigittaRestMvFilteredSplit,'Brigitta rest fft');
-% 
-% %holding breath for 30 seconds: Adam 
-% adamHoldBreath30Sec=hhmbinread('hold_breath/adam_hold_breath_30sec.hhm');
-% adamHoldBreath30SecFiltered = butterworthFilter(adamHoldBreath30Sec.ecg1 - mean(adamHoldBreath30Sec.ecg1),5,20,0.5);
-% frequencySpectrum(adamHoldBreath30SecFiltered,'Adam - holding breath for 30 seconds - fft'); %visualize in frequency domain 
-%  
-% %holding breath for 30 seconds: Zsofia 
-% zsofiaHoldBreath30Sec=hhmbinread('hold_breath/zsofia_hold_breath_30sec.hhm');
-% zsofiaHoldBreath30SecFiltered = butterworthFilter(zsofiaHoldBreath30Sec.ecg1 - mean(zsofiaHoldBreath30Sec.ecg1),5,20,0.5);
-% frequencySpectrum(zsofiaHoldBreath30SecFiltered,'Zsofia - holding breath for 30 seconds - fft'); %visualize in frequency domain 
+%% 2/a
+% ekg signals
+% holding breath for 30 seconds: Brigitta
+brigittaHoldBreath30Sec=hhmbinread('hold_breath/brigitta_hold_breath_30sec.hhm');
+brigittaHoldBreath30SecFiltered = butterworthFilter(brigittaHoldBreath30Sec.ecg1,5,20,0.5);
+frequencySpectrum(brigittaHoldBreath30SecFiltered,'Brigitta - holding breath for 30 seconds - filtered EKG frequency spectrum (original length)'); %visualize in frequency domain 
 
-% %holding breath for 30 seconds: Brigitta
- brigittaHoldBreath30Sec=hhmbinread('hold_breath/brigitta_hold_breath_30sec.hhm');
-% brigittaHoldBreath30SecFiltered = butterworthFilter(brigittaHoldBreath30Sec.ecg1 - mean(brigittaHoldBreath30Sec.ecg1),5,20,0.5);
-% frequencySpectrum(brigittaHoldBreath30SecFiltered,'Brigitta - holding breath for 30 seconds - fft'); %visualize in frequency domain 
+% controlling breathing: Brigitta
+brigittaControlBreath=hhmbinread('control_breath/brigitta_control_breath.hhm');
+brigittaControlBreathFiltered = butterworthFilter(brigittaControlBreath.ecg1,5,20,0.5);
+frequencySpectrum(brigittaControlBreathFiltered,'Brigitta - controlled breathing - filtered EKG frequency spectrum (original length)'); %visualize in frequency domain
 
-% %controlling breathing: Adam 
-% adamControlBreath=hhmbinread('control_breath/adam_control_breath.hhm');
-% adamControlBreathFiltered = butterworthFilter(adamControlBreath.ecg1 - mean(adamControlBreath.ecg1),5,20,0.5);
-% frequencySpectrum(adamControlBreathFiltered,'Adam - controlled breathing - fft'); %visualize in frequency domain 
-%  
-% %controlling breathing: Zsofia 
-% zsofiaControlBreath=hhmbinread('control_breath/zsofia_control_breath.hhm');
-% zsofiaControlBreathFiltered = butterworthFilter(zsofiaControlBreath.ecg1 - mean(zsofiaControlBreath.ecg1),5,20,0.5);
-% frequencySpectrum(zsofiaControlBreathFiltered,'Zsofia - controlled breathing - fft'); %visualize in frequency domain 
-% 
-%controlling breathing: Brigitta
- brigittaControlBreath=hhmbinread('control_breath/brigitta_control_breath.hhm');
-% brigittaControlBreathFiltered = butterworthFilter(brigittaControlBreath.ecg1 - mean(brigittaControlBreath.ecg1),5,20,0.5);
-% frequencySpectrum(brigittaControlBreathFiltered,'Brigitta - controlled breathing - fft'); %visualize in frequency domain
-% 
-% %Physical activity: Brigitta
- brigittaPhysicalActivity=hhmbinread('squat/brigitta_squat.hhm');
-% brigittaPhysicalActivityFiltered = butterworthFilter(brigittaPhysicalActivity.ecg1 - mean(brigittaPhysicalActivity.ecg1),5,20,0.5);
-% frequencySpectrum(brigittaPhysicalActivityFiltered,'Brigitta - physical activity - fft'); %visualize in frequency domain
+% physical activity: Brigitta
+brigittaPhysicalActivity=hhmbinread('squat/brigitta_squat.hhm');
+brigittaPhysicalActivityFiltered = butterworthFilter(brigittaPhysicalActivity.ecg1,5,20,0.5);
+frequencySpectrum(brigittaPhysicalActivityFiltered,'Brigitta - physical activity - filtered EKG frequency spectrum (original length)'); %visualize in frequency domain
 
-% %% 2 task
-
+% ppgl signals
 fs=1000; % sampled at this frequency [Hz]
 
-% load red ppgl signal into variables
+%load red ppgl RED signals into variables and split them
 brigittaRestPpglRed=brigittaRest.ppgl_red;
-brigittaRestPpglRedSplit = brigittaRestPpglRed(10000:length(brigittaRestPpglRed)); % be aware of matrix size
+brigittaRestPpglRedSplit = brigittaRestPpglRed(10000:length(brigittaRestPpglRed));
 
 brigittaHoldBreathPpglRed = brigittaHoldBreath30Sec.ppgl_red;
 brigittaHoldBreathPpglRedSplit = brigittaHoldBreathPpglRed(10000:length(brigittaHoldBreathPpglRed));
 
-brigittaSquat = hhmbinread('squat/brigitta_squat.hhm'); % this file was not loaded
-brigittaSquatPpglRed = brigittaSquat.ppgl_red;
-brigittaSquatPpglRedSplit = brigittaSquatPpglRed(10000:length(brigittaSquatPpglRed));
+brigittaPhysicalActivityPpglRed = brigittaPhysicalActivity.ppgl_red;
+brigittaPhysicalActivityPpglRedSplit = brigittaPhysicalActivityPpglRed(10000:length(brigittaPhysicalActivityPpglRed));
 
 brigittaControlBreathPpglRed = brigittaControlBreath.ppgl_red;
 brigittaControlBreathPpglRedSplit = brigittaControlBreathPpglRed(10000:length(brigittaControlBreathPpglRed));
 
-% visualize signals
-figure()
+% visualize ppgl RED signals
+figure();
 
 t=(1:length(brigittaRestPpglRedSplit))/fs;
-subplot(3,1,1); 
-plot(t,3.3 / 8192 *(brigittaRestPpglRedSplit - 2048)); title('brigitta rest ppgl red'); % same rescaling used as in the beginning, be aware of HHMD version
-xlabel('t [sec]');ylabel('U [mV]');
+subplot(6,1,1); 
+plot(t,toMillivolt(brigittaRestPpglRedSplit));
+title('Brigitta rest - red split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
 
 t=(1:length(brigittaHoldBreathPpglRedSplit))/fs;
-subplot(3,1,2); 
-plot(t,3.3 / 8192 *(brigittaHoldBreathPpglRedSplit - 2048)); title('brigitta holding breath ppgl red');
+subplot(6,1,2); 
+plot(t,toMillivolt(brigittaHoldBreathPpglRedSplit));
+title('Brigitta holding breath - red split ppgl signal');
 xlabel('t [sec]');ylabel('U [mV]');
 
-figure()
+figure();
 
 t=(1:length(brigittaControlBreathPpglRedSplit))/fs;
-subplot(4,1,1); 
-plot(t,3.3 / 8192 *(brigittaControlBreathPpglRedSplit - 2048)); title('brigitta controlling breathing ppgl red');
+subplot(7,1,1); 
+plot(t,toMillivolt(brigittaControlBreathPpglRedSplit));
+title('Brigitta controlling breathing - red split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
+
+t=(1:length(brigittaPhysicalActivityPpglRedSplit))/fs;
+subplot(7,1,2); 
+plot(t,toMillivolt(brigittaPhysicalActivityPpglRedSplit));
+title('Brigitta squat - red split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
+
+% load INFRARED (NIR) ppgl signals into variables 
+brigittaRestPpglNir=brigittaRest.ppgl_nir;
+brigittaRestPpglNirSplit = brigittaRestPpglNir(10000:length(brigittaRestPpglNir));
+
+brigittaHoldBreathPpglNir = brigittaHoldBreath30Sec.ppgl_nir;
+brigittaHoldBreathPpglNirSplit = brigittaHoldBreathPpglNir(10000:length(brigittaHoldBreathPpglNir));
+ 
+brigittaPhysicalActivityPpglNir = brigittaPhysicalActivity.ppgl_nir;
+brigittaPhysicalActivityPpglNirSplit = brigittaPhysicalActivityPpglNir(10000:length(brigittaPhysicalActivityPpglNir));
+ 
+brigittaControlBreathPpglNir = brigittaControlBreath.ppgl_nir;
+brigittaControlBreathPpglNirSplit = brigittaControlBreathPpglNir(10000:length(brigittaControlBreathPpglNir));
+
+% visualize ppgl INFRARED (NIR) signals
+figure();
+
+t=(1:length(brigittaRestPpglNirSplit))/fs;
+subplot(6,1,1); 
+plot(t,toMillivolt(brigittaRestPpglRedSplit));
+title('Brigitta rest - Nir split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
+
+t=(1:length(brigittaHoldBreathPpglNirSplit))/fs;
+subplot(6,1,2); 
+plot(t,toMillivolt(brigittaHoldBreathPpglNirSplit));
+title('Brigitta holding breath - Nir split ppgl signal');
 xlabel('t [sec]');ylabel('U [mV]');
 
-t=(1:length(brigittaSquatPpglRedSplit))/fs;
-subplot(4,1,2); 
-plot(t,3.3 / 8192 *(brigittaSquatPpglRedSplit - 2048)); title('brigitta squat ppgl red');
-xlabel('t [sec]');ylabel('U [mV]');
+figure();
 
-% % load infrared ppgl signal into variables 
-% adamRestPpglInfrared=adamRest.ppgl_nir;
-% adamRestPpglInfraredSplit = adamRestPpglInfrared(1:30000); % be aware of matrix size
-% 
-% adamHoldBreathPpglInfrared = adamHoldBreath30Sec.ppgl_nir;
-% adamHoldBreathPpglInfraredSplit = adamHoldBreathPpglInfrared(1:30000);
-% 
-% adamControlBreathPpglInfrared = adamControlBreath.ppgl_nir;
-% adamControlBreathPpglInfraredSplit = adamControlBreathPpglInfrared(1:30000);
-% 
-% adamSquat = hhmbinread('squat/adam_squat.hhm'); % this file was not loaded
-% adamSquatPpglInfrared = adamSquat.ppgl_nir;
-% adamSquatPpglInfraredSplit = adamSquatPpglInfrared(1:30000);
+t=(1:length(brigittaControlBreathPpglNirSplit))/fs;
+subplot(7,1,1); 
+plot(t,toMillivolt(brigittaControlBreathPpglNirSplit));
+title('Brigitta controlling breathing - Nir split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
 
-% % visualize signals
-% figure()
-% 
-% t=(1:length(brigittaRestPpglInfraredSplit))/fs;
-% subplot(5,1,1); 
-% plot(t,3.3 / 4096 *(brigittaRestPpglInfraredSplit - 2048)); title('brigitta rest ppgl infrared'); % same rescaling used as in the beginning, be aware of HHMD version
-% xlabel('t [sec]');ylabel('U [mV]');
-% 
-% t=(1:length(brigittaHoldBreathPpglInfraredSplit))/fs;
-% subplot(5,1,2); 
-% plot(t,3.3 / 4096 *(brigittaHoldBreathPpglInfraredSplit - 2048)); title('brigitta holding breath ppgl infrared');
-% xlabel('t [sec]');ylabel('U [mV]');
-% 
-% t=(1:length(brigittaControlBreathPpglInfraredSplit))/fs;
-% subplot(6,1,1); 
-% plot(t,3.3 / 4096 *(brigittaControlBreathPpglInfraredSplit - 2048)); title('brigitta controlling breathing ppgl infrared');
-% xlabel('t [sec]');ylabel('U [mV]');
-% 
-% t=(1:length(brigittaSquatPpglInfraredSplit))/fs;
-% subplot(6,1,2); 
-% plot(t,3.3 / 4096 *(brigittaSquatPpglInfraredSplit - 2048)); title('brigitta squat ppgl infrared');
-% xlabel('t [sec]');ylabel('U [mV]');
+t=(1:length(brigittaPhysicalActivityPpglNirSplit))/fs;
+subplot(7,1,2); 
+plot(t,toMillivolt(brigittaPhysicalActivityPpglNirSplit));
+title('Brigitta squat - Nir split ppgl signal');
+xlabel('t [sec]');
+ylabel('U [mV]');
+
+function out = toMillivolt(ECGsignal)
+    out=3.3/8192*(ECGsignal-2048);
+end
 
 %% Frequency spectrum
 function frequencySpectrum(ecg,titleOfDiagram)
@@ -224,7 +215,7 @@ function frequencySpectrum(ecg,titleOfDiagram)
     ylabel('amplification')
     xlim([0 10]) % X axis
 end
-% 
+
 %% Butterworth filter
 function highPassFilteredSignal=butterworthFilter(ecg, order, lowerCutOff,upperCutOff)
     % @ecg: hhm object's property
